140750bc302a1c8bebb73c46364926d9c79c5f54
Neale Johnston <neale@team-johnston.com>
2021-02-01 11:47:39 +0000

    remove unused import

--- src/containers/OrderingContainer.js
+++ src/containers/OrderingContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useInterval } from '../hooks/useInterval';
 import OrderList from '../components/OrderList';
 
