b4ac5df7ebed8143afd6e2978611e298fbbff039
Neale Johnston <neale@team-johnston.com>
2021-02-01 11:45:21 +0000

    added useInterval hook for polling

--- src/containers/OrderingContainer.js
+++ src/containers/OrderingContainer.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useInterval } from '../hooks/useInterval';
 import OrderList from '../components/OrderList';
 
 const OrderingContainer = () => {
     const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
+    useInterval(() => {
         fetch("http://localhost:8080/orders")
         .then(res => res.json())
         .then((data) => setOrders(data))
-    }, []);
+    }, 10000);
 
 
     return(
