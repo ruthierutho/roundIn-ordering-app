ccf12ce2d632fbb5aaf0f65f8bee5ca6f8ed93d3
Neale Johnston <neale@team-johnston.com>
2021-02-02 09:29:44 +0000

    tidy up

--- src/containers/OrderingContainer.js
+++ src/containers/OrderingContainer.js
@@ -1,39 +1,36 @@
-import React, { useState } from 'react';
-import { useInterval } from '../hooks/useInterval';
-import OrderList from '../components/OrderList';
-import OrderDetails from '../components/OrderDetails';
-import '../static/ordering-container.css'
+import React, { useState } from "react";
+import { useInterval } from "../hooks/useInterval";
+import OrderList from "../components/OrderList";
+import OrderDetails from "../components/OrderDetails";
+import "../static/ordering-container.css";
 
 const OrderingContainer = () => {
-    const [orders, setOrders] = useState([]);
-    const [selectedOrder, setSelectedOrder] = useState();
+  const [orders, setOrders] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState();
 
-    useInterval(() => {
-        fetch("http://localhost:8080/orders")
-        .then(res => res.json())
-        .then((data) => setOrders(data))
-    }, 10000);
+  useInterval(() => {
+    fetch("http://localhost:8080/orders")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, 10000);
 
-    const showDetails = (orderKey) => {
-        const order = orders[orderKey]
-        setSelectedOrder(order)
-        
-    }
+  const showDetails = (orderKey) => {
+    const order = orders[orderKey];
+    setSelectedOrder(order);
+  };
 
-    const hideDetails = () => {
-        
-        setSelectedOrder(null)
-    }
+  const hideDetails = () => {
+    setSelectedOrder(null);
+  };
 
-    return(
-        <>
-        <h1>Rounders</h1>
-        <h3 className="venue-welcome">Welcome Venue Name!</h3>
-        <OrderList orders={orders} showDetails={showDetails}></OrderList>
-        <OrderDetails selectedOrder={selectedOrder} hideDetails={hideDetails}/>
-
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Rounders</h1>
+      <h3 className="venue-welcome">Welcome Venue Name!</h3>
+      <OrderList orders={orders} showDetails={showDetails}></OrderList>
+      <OrderDetails selectedOrder={selectedOrder} hideDetails={hideDetails} />
+    </>
+  );
+};
 
 export default OrderingContainer;
