5e89a4b55c917f941bd1e702c10927602f4444d8
Neale Johnston <neale@team-johnston.com>
2021-02-04 15:20:09 +0000

    added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Logo from "../static/images/GPM_RI_logo1.svg";
+import "../static/header.css";
 
 const Header = () => {
-  return <h1>roundIn</h1>;
+  return <img id="logo" src={Logo} alt="Logo" />;
 };
 
 export default Header;
